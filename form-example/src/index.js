8413f3af6d4f0427ef0fcf9cd422dceb928f2bda
Nils Hartmann <nils@nilshartmann.net>
2020-05-10 18:23:50 +0200

    Add FormExample

---
+++ form-example/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import JobSearchFormWithState from "./App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <JobSearchFormWithState />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
