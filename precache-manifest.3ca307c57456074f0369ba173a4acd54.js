80ba95e9fd288ada1860c06d58d722a10631a9b1
Nils Hartmann <nils@nilshartmann.net>
2020-10-18 16:07:04 +0200

    Updates

---
+++ precache-manifest.3ca307c57456074f0369ba173a4acd54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fb88df420186519cdac76ab5518bcf1",
+    "url": "/myapp/react-statemanagement-examples/frontend/index.html"
+  },
+  {
+    "revision": "415c58cee9bbfd563632",
+    "url": "/myapp/react-statemanagement-examples/frontend/static/css/main.06e37daa.chunk.css"
+  },
+  {
+    "revision": "22c8cc536f8565467e97",
+    "url": "/myapp/react-statemanagement-examples/frontend/static/js/2.b1c708d2.chunk.js"
+  },
+  {
+    "revision": "2f77b71e3ccea97bea270bf306f0e4b9",
+    "url": "/myapp/react-statemanagement-examples/frontend/static/js/2.b1c708d2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "415c58cee9bbfd563632",
+    "url": "/myapp/react-statemanagement-examples/frontend/static/js/main.92e6211b.chunk.js"
+  },
+  {
+    "revision": "9d4e72005bb5bb54578c",
+    "url": "/myapp/react-statemanagement-examples/frontend/static/js/runtime-main.e7c87416.js"
+  }
+]);
